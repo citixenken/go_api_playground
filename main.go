74903c1087e48ed4de75f4db250f1d879e7f6cb3
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-04-06 07:57:23 +0300

    imports and data setup

--- main.go
+++ main.go
@@ -1,2 +1,11 @@
 package main
 
+/*store book data in memory => struct tags ie `json:"title"`
+specify shape when content is serialized into JSON */
+type book struct {
+	// capitalization makes it an exported field/public field
+	ID string `json:"id"`
+	Title string `json:"title"`
+	Author string `json:"author"`
+	Quantity int `json:"quantity"`
+}
