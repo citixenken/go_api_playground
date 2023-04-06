3768d95f18f663956406f470400008e12581bc5a
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-04-06 08:00:21 +0300

    gin router initialization

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+)
+
 /*store book data in memory => struct tags ie `json:"title"`
 specify shape when content is serialized into JSON */
 type book struct {
@@ -8,4 +12,15 @@ type book struct {
 	Title string `json:"title"`
 	Author string `json:"author"`
 	Quantity int `json:"quantity"`
+}
+
+// books slice to seed record book data.
+var books = []book{
+	{ID: "1", Title: "Dune", Author: "Frank Herbert", Quantity: 5},
+    {ID: "2", Title: "Fahrenheit 451", Author: "Ray Bradbury", Quantity: 3},
+    {ID: "3", Title: "Hail Mary", Author: "Andy Weir", Quantity: 14},
+}
+
+func main (){
+	router := gin.Default()
 }
